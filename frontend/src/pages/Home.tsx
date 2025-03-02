3b92f652f8816578b5f6521864f464c98b162781
mpavithran <mpavithran1998@gmail.com>
2025-03-02 12:10:09 +0530

    setup: socket client connection

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
 import DrawCanva from "../components/DrawCanva";
+import { io, Socket } from "socket.io-client";
 
+const socket: Socket = io("http://localhost:3001/v1");
 const Home = () => {
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected server");
+    });
+
+    socket.emit("ping", "Hello");
+    socket.on("pong", (data) => {
+      console.log(data);
+    });
+  }, [socket.connected]);
+
   return (
     <div className="w-[100vw]">
       <h1 className="font-bold text-xl text-center mb-4">Draw App</h1>
